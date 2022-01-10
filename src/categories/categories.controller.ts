6d8b6caceb5d6c40b61f40c6a82891ead4a7ab77
Gabriel Oliveira <gabrieloliveiravicente2015@gmail.com>
2022-01-10 09:39:38 -0300

    Removendo operações inexistentes do controller (findAll, findOne)

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -12,16 +12,6 @@ export class CategoriesController {
     return this.categoriesService.create(createCategoryDto);
   }
 
-  @Get()
-  findAll() {
-    return this.categoriesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(+id);
-  }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoriesService.update(+id, updateCategoryDto);
