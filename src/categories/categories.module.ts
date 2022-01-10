6efedff79c67d351476b7ff5eea9e9e14346f50e
Gabriel Oliveira <gabrieloliveiravicente2015@gmail.com>
2022-01-10 11:56:10 -0300

    configurando o module (adicionando o PrismaModule e PrismService)

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports:[PrismaModule],
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService, PrismaService]
 })
 export class CategoriesModule {}
